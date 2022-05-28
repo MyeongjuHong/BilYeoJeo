5661ddb624029ebc5819d7017b999d70da44464f
Myeongju Hong <hmjpearl@gmail.com>
2022-05-28 23:01:03 +0900

    feat: Add react springboot connect code

---
+++ bilyeojeo/src/main/java/com/capstone/bilyeojeo/BilyeojeoApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.bilyeojeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilyeojeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilyeojeoApplication.class, args);
+	}
+
+}
---
+++ bilyeojeo/src/test/java/com/capstone/bilyeojeo/BilyeojeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.bilyeojeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilyeojeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
